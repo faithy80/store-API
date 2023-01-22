f12bcd0c9c95eeb05210e33b1c3194b5881d0c86
Krisztian Buza <krisztian.buza80@gmail.com>
2023-01-22 19:41:14 +0000

    Dotenv added.

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 // setup environment
-const port = process.env.port || 3000;
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // setup express app
 const app = express();
 app.use(express.json());
+mongoose.set('strictQuery', true);
+const port = process.env.PORT || 3000;
 
 // routes
 app.get('/', (req, res) => {
     return res.json({message: 'Hello World!'});
 });
 
-// setup the start of the server
-const start = async () => {
-    try {
-        await mongoose.connect(
-            'mongodb://localhost:27017/storeDB'
-        );
-        app.listen(port, () => {
-            console.log(`Server started on port ${port}.`);
-        });
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-};
+// connect to DB and start the server
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to DB.');
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
 
-// start the server
-start();
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
+});
