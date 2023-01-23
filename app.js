5af8abd7c2ad63c342c18b622b398fc2b0fb15a0
Krisztian Buza <krisztian.buza80@gmail.com>
2023-01-23 20:56:52 +0000

    API model added to app.js.

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+
 // setup express app
 const app = express();
 app.use(express.json());
+
 mongoose.set('strictQuery', true);
+
+const { Item } = require("./models");
+
 const port = process.env.PORT || 3000;
+
 
 // routes
 app.get('/', (req, res) => {
     return res.json({message: 'Hello World!'});
 });
+
+
+// API routes
+app.get("/items", async (req, res) => {
+    const allItems = await Item.find();
+    return res.status(200).json(allItems);
+});
+
 
 // connect to DB and start the server
 mongoose
